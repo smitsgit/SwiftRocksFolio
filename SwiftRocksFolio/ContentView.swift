c93990b713ec099199a3b0dd50a624a55c7c51b1
Smital Desai <desaismital@gmail.com>
2021-06-02 09:31:54 +0530

    Add tabview to check the add and delete functionality

--- SwiftRocksFolio/ContentView.swift
+++ SwiftRocksFolio/ContentView.swift
@@ -9,27 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        TabView {
+            AddView().tabItem {
+                Text("Add")
+            }
+            ProjectsView(showClosedProjects: false)
+                    .tabItem {
+                        Image(systemName: "list.bullet")
+                        Text("Open")
+                    }
+
+            ProjectsView(showClosedProjects: true)
+                    .tabItem {
+                        Image(systemName: "checkmark")
+                        Text("Closed")
+                    }
+        }
+    }
+}
+
+struct AddView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Project.entity(), sortDescriptors: []) var projects: FetchedResults<Project>
     var body: some View {
-        VStack {
-            List {
-                ForEach(projects, id: \.self) { item in
-                    Text(item.title ?? "Unknown")
-                }
-            }
+        NavigationView {
+            VStack {
+                Button("Tap me") {
+                    for i in 1...5 {
+                        let project = Project(context: self.moc)
+                        project.title = "Project \(i)"
+                        project.items = []
+                        project.creationDate = Date()
+                        project.closed = Bool.random()
 
-            Button("Tap me") {
-                for i in 1...5 {
-                    let project = Project(context: self.moc)
-                    project.title = "Project \(i)"
-                    project.items = []
-                    project.creationDate = Date()
-                    project.closed = Bool.random()
+                        for j in 1...10 {
+                            let item = Item(context: self.moc)
+                            item.title = "Item \(j)"
+                            item.creationDate = Date()
+                            item.completed = Bool.random()
+                            item.project = project
+                            item.priority = Int16.random(in: 1...3)
+                        }
+                    }
+                    try? self.moc.save()
                 }
 
-                try? self.moc.save()
-            }
+                Button("Delete me") {
+                    for project in self.projects {
+                        self.moc.delete(project)
+                    }
+
+                    try? self.moc.save()
+                }
+            }.navigationBarTitle("Add me")
+
         }
     }
 }
---
+++ SwiftRocksFolio/ProjectsView.swift
@@ -0,0 +1,39 @@
+//
+//  ProjectsView.swift
+//  SwiftRocksFolio
+//
+//  Created by Smital on 02/06/21.
+//  Copyright © 2021 IoSmit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    @Environment(\.managedObjectContext) var moc
+    let showClosedProjects: Bool
+    let projects: FetchRequest<Project>
+
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        projects = FetchRequest<Project>(entity: Project.entity(), sortDescriptors: [
+            NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)
+        ], predicate: NSPredicate(format: "closed = %d", showClosedProjects))
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(projects.wrappedValue, id: \.self) { project in
+                    Section(header: Text(project.title ?? "")) {
+                        ForEach(project.items?.allObjects as? [Item] ?? [], id:\.self) { item in
+                            Text(item.title ?? "None")
+                        }
+                    }
+                }
+            }.navigationBarTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+        }
+    }
+}
+
+
+
